84e40a4e86a3eefa11adf11a6b893d80f6a06795
meg-netswitch <meghan.granit@netswitch.ai>
2024-03-12 15:49:32 -0400

    Initial Commit

---
+++ TemplateApp/TemplateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateAppApp.swift
+//  TemplateApp
+//
+//  Created by Meghan Granit on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TemplateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
